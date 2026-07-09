1e4eac7c095f9552e58a0bca0039e00de58521cd
Salman9193 <salman.vnit@gmail.com>
2026-07-09 21:11:21 +0530

    Add Max Points on a Line (#149) with 3SUM-hard research tab: scripts/generate_revision_page.py

--- scripts/generate_revision_page.py
+++ scripts/generate_revision_page.py
@@ -43,6 +43,7 @@ LC = {
     "sort-characters-by-frequency":(451,"sort-characters-by-frequency"),
     "flatten-a-multilevel-doubly-linked-list":(430,"flatten-a-multilevel-doubly-linked-list"),
     "reverse-bits":(190,"reverse-bits"),
+    "max-points-on-a-line":(149,"max-points-on-a-line"),
     "find-pivot-index":(724,"find-pivot-index"),
     "sliding-window-maximum":(239,"sliding-window-maximum"),
     "car-pooling":(1094,"car-pooling"),
