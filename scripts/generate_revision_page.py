4e24bf0e29a0f4d83f98fd9eae2b9b84f1924f83
Salman9193 <salman.vnit@gmail.com>
2026-07-09 07:26:52 +0530

    Add Palindrome Linked List (#234): scripts/generate_revision_page.py

--- scripts/generate_revision_page.py
+++ scripts/generate_revision_page.py
@@ -39,6 +39,7 @@ LC = {
     "product-of-array-except-self":(238,"product-of-array-except-self"),
     "maximum-of-absolute-value-expression":(1131,"maximum-of-absolute-value-expression"),
     "shortest-common-supersequence":(1092,"shortest-common-supersequence"),
+    "palindrome-linked-list":(234,"palindrome-linked-list"),
     "find-pivot-index":(724,"find-pivot-index"),
     "sliding-window-maximum":(239,"sliding-window-maximum"),
     "car-pooling":(1094,"car-pooling"),
