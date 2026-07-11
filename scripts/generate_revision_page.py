3a9695e4c3fa0075c896ac0095bc4b5fd3660e4b
Salman9193 <salman.vnit@gmail.com>
2026-07-12 01:50:57 +0530

    Add Combination Sum II (#40): backtracking + dedupe, Research tab, cross-link to #39: scripts/generate_revision_page.py

--- scripts/generate_revision_page.py
+++ scripts/generate_revision_page.py
@@ -45,6 +45,7 @@ LC = {
     "reverse-bits":(190,"reverse-bits"),
     "max-points-on-a-line":(149,"max-points-on-a-line"),
     "largest-divisible-subset":(368,"largest-divisible-subset"),
+    "combination-sum-ii":(40,"combination-sum-ii"),
     "find-pivot-index":(724,"find-pivot-index"),
     "sliding-window-maximum":(239,"sliding-window-maximum"),
     "car-pooling":(1094,"car-pooling"),
